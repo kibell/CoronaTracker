8ea9780a7047b8a6d43555552f457863d181bb7a
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-27 11:01:03 -0600

    added all countries dynamically to the browser using the api and await async

--- corona_tracker/src/components/CountryPicker/Country.js
+++ corona_tracker/src/components/CountryPicker/Country.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import { NativeSelect, FormControl } from '@material-ui/core'
+import { StylesContext } from '@material-ui/styles'
+
+import { fetchedCountries, fetchCountries } from '../../api';
 
 const CountryPicker = () => {
-    return(
-        <h1>
+    const [fetchedCountries, setFetchedCountries] = useState([])
 
-        </h1>
+useEffect(() => {
+    const fetchAPI = async () => {
+        setFetchedCountries(await fetchCountries());
+    }
+
+    fetchAPI()
+}, [setFetchedCountries])
+
+
+
+    return(
+        <FormControl className={StylesContext.formControl}>
+            <NativeSelect>
+                <option value ='global'>Global</option>
+                {fetchedCountries.map((country, i) => <option key={i}  value={country}>{country}</option>)}
+            </NativeSelect>
+
+        </FormControl>
     )
 }
+
 
 export default CountryPicker
