e5c0901a0cb0402f5778b804aec50c8aaef2a0f5
Kitrick Bell <kitrickd.bell@gmail.com>
2021-11-27 09:10:22 -0600

    added line chart to the page, installed chart js. fetched data and called it in the front end

--- corona_tracker/src/components/Charts/chart.js
+++ corona_tracker/src/components/Charts/chart.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { fetchDailyData } from "../../api";
+import { Line, Bar } from "react-chartjs-2";
 
+import styles from "../Charts/chart.css";
 const Chart = () => {
-    return(
-        <h1>
+  const [dailyData, setDailyData] = useState([]);
 
-        </h1>
-    )
-}
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setDailyData(await fetchDailyData());
+    };
+    console.log(dailyData);
+    fetchAPI();
+  });
 
-export default Chart
+  const lineChart = dailyData.length
+   ? (
+    <Line
+      data={{
+        labels: dailyData.map(({ date }) => date),
+        datasets: [{
+            data: dailyData.map(({confirmed })=> confirmed),
+            label:'Infected',
+            borderColor:'blue',
+            fill:true
+        }, {data: dailyData.map(({deaths })=> deaths),
+        label:'Deaths',
+        borderColor:'red',
+        backgroundColor: 'rgba(255,0,0,0.5)',
+        fill: true
+        }],
+      }}
+    />
+  ) : null;
+
+  return(
+<div className={styles.container}>
+    {lineChart}
+</div>
+
+  ) 
+};
+
+export default Chart;
